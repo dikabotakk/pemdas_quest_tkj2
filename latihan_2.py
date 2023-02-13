c3d4e6f55216c8e51e5fb7134b734f6294a1255b
dikabotakk <111480093+dikabotakk@users.noreply.github.com>
2023-02-13 09:55:49 +0700

    Add files via upload

---
+++ latihan_2.py
@@ -0,0 +1,17 @@
+'''
+buatlah program menghitung luas bangun datarmenggunakan fungsi.
+- segitiga
+- persegi panjang
+'''
+
+def luas_s(alas, tinggi):
+    luas = 1/2 * alas * tinggi
+    print("luas segitiga adalah", luas)
+
+luas_s(6,5)
+
+def luas_pp(panjang, lebar):
+    luas = panjang * lebar
+    print("luas persegi panjang adalah", luas)
+
+luas_pp(23,14)
---
+++ latihan_fungsi.py
@@ -0,0 +1,40 @@
+'''fungsi pada python didefinisikan dengan def'''
+
+'''def nama_fungsi():'''
+
+def salam():
+    print("assalamu'alaikum !! akhi ukhty")
+
+salam()
+salam()
+salam()
+
+'''buatlah sebuah fungsi versi kalian'''
+
+'''def nama_fungsi'''
+
+def takbir():
+    print("takbir'allahhuakbar")
+
+takbir()
+takbir()
+takbir()
+
+'''fungsi dengan parameter'''
+'''def nama_fungsi(p1, p2)'''
+
+def say_hi(nama):
+    print("hallo perkenalkan nama saya", nama)
+
+say_hi("jawir")
+say_hi("reog")
+
+'''buatlah fungsi dengan 3 parameter'''
+ 
+def welcome(nama,umur,kelas):
+    print("halo nama saya:",nama)
+    print("umur:",umur)
+    print("kelas:",kelas)
+
+welcome("andhika", "15", "10")
+
